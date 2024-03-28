f93a3776a7304d43fed41a9462c61dd0b77a9176
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-03-28 18:06:48 +0300

    Update Game.jsx

--- src/game.jsx
+++ src/game.jsx
@@ -1,15 +1,13 @@
 import "./Game.css"
 
 function Game(props){
-    const percentage = props.step/props.questions.length*100
-    Math.round(percentage)
-    console.log(percentage)
+    const percentage = Math.round(props.step/props.questions.length*100)
     return(
         <div>
             <div style={{width: "100%",backgroundColor: 'grey'}} className="progress">
                 <div style={{width: `${percentage}%`}}className="progress__inner"></div>
             </div>
-            <h1>{props.question["title"]}</h1>
+            <h1>{props.question.title}</h1>
             <ul>
                 {props.question.variants.map((item, index) => (
                     <li key={index} onClick={() => props. handleClick(index)}>{item}</li>
